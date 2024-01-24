8e850ac1ebed59ba4ff493e619f0a61002411858
MatheusMorata <matlucar09@hotmail.com>
2024-01-24 12:29:04 -0300

    feat readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Algoritmos
 <p>Este repositório contém todos os algoritmos desenvolvidos por mim durante minha leitura do livro <b>Entendendo Algoritmos - Um Guia Ilustrado Para Programadores e Outros Curiosos.</b>
-<br>Até o momento estou na página 162.</p>
+<br>Até o momento estou na página 304.</p>
--- tabelasHash.lua
+++ tabelasHash.lua
@@ -5,3 +5,8 @@ tabela = {Maca = 10.99,Laranja = 8.99,Pera = 2.99}
 print("Preco da maca: R$" .. tabela['Maca'])
 print("Preco da laranja: R$" .. tabela['Laranja'])
 print("Preco da pera: R$" .. tabela['Pera'])
+
+
+print("Preco da maca: R$" .. tabela.Maca)
+print("Preco da laranja: R$" .. tabela.Laranja)
+print("Preco da pera: R$" .. tabela.Pera)
